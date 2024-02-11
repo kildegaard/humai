aba076ff9fff4577c73e5ef6d12e703a9af0c95f
gus-laptop <gustavo.kildegaard@gmail.com>
2024-02-11 19:01:26 -0300

    primer programa en bash!

---
+++ 01 - Intro a Python/clase 5/bash/saludo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+nombre='Gustavo'
+apellido="Kildegaard"
+dia=$(date +%d)
+mes=$(date +%B)
+ano=$(date +%Y)
+day=$(date +%A)
+
+echo "Hola! Soy $nombre $apellido, y hoy es $day $dia de $mes del $ano"
